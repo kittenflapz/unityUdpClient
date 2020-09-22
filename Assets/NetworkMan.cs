1c755034a085962957474a26f6ffc24a7b9f53cb
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-09-21 20:20:29 -0400

    Setup and spawning functionality
    - Color changes on update based on server messages

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -9,12 +9,16 @@ using System.Net;
 public class NetworkMan : MonoBehaviour
 {
     public UdpClient udp;
+    public GameObject playerPrefab;
+    public List<PlayerCube> playersInGame;
+    public string myAddress;
+
     // Start is called before the first frame update
     void Start()
     {
         udp = new UdpClient();
         
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
+        udp.Connect("ec2-3-137-149-42.us-east-2.compute.amazonaws.com",12345);
 
         Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
       
@@ -43,17 +47,18 @@ public class NetworkMan : MonoBehaviour
     [Serializable]
     public class Player{
         public string id;
+
+        [Serializable]
         public struct receivedColor{
             public float R;
             public float G;
             public float B;
         }
-        public receivedColor color;        
+        public receivedColor color;
     }
 
     [Serializable]
     public class NewPlayer{
-        
     }
 
     [Serializable]
@@ -62,7 +67,7 @@ public class NetworkMan : MonoBehaviour
     }
 
     public Message latestMessage;
-    public GameState lastestGameState;
+    public GameState latestGameState;
     void OnReceived(IAsyncResult result){
         // this is what had been passed into BeginReceive as the second parameter:
         UdpClient socket = result.AsyncState as UdpClient;
@@ -81,9 +86,10 @@ public class NetworkMan : MonoBehaviour
         try{
             switch(latestMessage.cmd){
                 case commands.NEW_CLIENT:
+                    // When a new player is connected, the client adds the details of this player into a list of currently connected players.
                     break;
                 case commands.UPDATE:
-                    lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                    latestGameState = JsonUtility.FromJson<GameState>(returnData);
                     break;
                 default:
                     Debug.Log("Error");
@@ -98,12 +104,47 @@ public class NetworkMan : MonoBehaviour
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
+    void SpawnPlayers()
+    {
+        if (playersInGame.Count == 0)
+        {
+            foreach (Player player in latestGameState.players) // go through each player the server says we have
+            {
+                Vector3 randomPos = new Vector3(UnityEngine.Random.Range(-3f, 3f), UnityEngine.Random.Range(-3f, 3f), UnityEngine.Random.Range(-3f, 3f));
+                GameObject newPlayer = Instantiate(playerPrefab, randomPos, Quaternion.identity);
+                newPlayer.GetComponent<PlayerCube>().networkID = player.id;
+                playersInGame.Add(newPlayer.GetComponent<PlayerCube>());
+            }
+        }
 
+        foreach (Player player in latestGameState.players) // go through each player the server says we have
+        {
+ 
+            foreach (PlayerCube playerCube in playersInGame) // go through each cube we have made
+            {
+                if (player.id != playerCube.networkID) // if there isn't already a cube with this network id, spawn it
+                {
+                    Vector3 randomPos = new Vector3(UnityEngine.Random.Range(-3f, 3f), UnityEngine.Random.Range(-3f, 3f), UnityEngine.Random.Range(-3f, 3f));
+                    GameObject newPlayer = Instantiate(playerPrefab, randomPos, Quaternion.identity);
+                    newPlayer.GetComponent<PlayerCube>().networkID = player.id;
+                    playersInGame.Add(newPlayer.GetComponent<PlayerCube>());
+                }
+            }
+        }
     }
 
-    void UpdatePlayers(){
-
+    void UpdatePlayers()
+    {
+        foreach (Player player in latestGameState.players) // go through each player the server says we have
+        {
+            foreach (PlayerCube playerCube in playersInGame) // go through each cube we have made
+            {
+                if (player.id == playerCube.networkID) // if the player id and the cube id match
+                {
+                    playerCube.cubeColor = new Color(player.color.R, player.color.G, player.color.B);
+                }
+            }
+        }
     }
 
     void DestroyPlayers(){
---
+++ Assets/PlayerCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCube : MonoBehaviour
+{
+    public string networkID;
+    public Color cubeColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("color: " + cubeColor);
+        GetComponent<Renderer>().material.color = cubeColor;
+    }
+}
